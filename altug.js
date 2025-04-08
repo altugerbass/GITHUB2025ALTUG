9d74d57322e5720f2e5ff0ebcc28fe84c3511018
altugerbass <altugerbas9@gmail.com>
2025-04-08 21:13:54 +0300

    08/04/2025 saat 21:13 de dosyalar düzeltildi ve gereksizler silindi

--- altug.js
+++
@@ -1,9 +0,0 @@
-let sehir = istanbul;
-let yas = 25;
-let evliMi = false;
-let cocukSayisi = 0;
-let yasHesapla = function(dogumYili) {
-    return new Date().getFullYear() - dogumYili;
-}
-let ad = ahmet;
-
