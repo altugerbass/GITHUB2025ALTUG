71e852b22206e6039914159bb664bdbdccd4527b
altugerbass <altugerbas9@gmail.com>
2025-04-08 13:05:22 +0300

    yeni şeyler

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ let ad = ahmet;
 let soyad = yılmaz;
 let yas = 25;
 let meslek = developer;
+let sehir = istanbul;
+let evliMi = false;
+let cocukSayisi = 0;
+let yasHesapla = function(dogumYili) {
+    return new Date().getFullYear() - dogumYili;
+}
