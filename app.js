870872ca2c6181c8c8a166187207e692055d8910
altugerbass <altugerbas9@gmail.com>
2025-04-09 19:55:05 +0300

    .

---
+++ app.js
@@ -0,0 +1,16 @@
+let adınız = "altug";
+let soyadınız = "erbas";
+let doğumYeri = "ankara";
+let medeniDurum = "bekar";
+let yaşınız = "19";
+let doğumYılınız = "2006";
+let doğumTarihiniz = "22-03-2006";
+console.log(adınız);
+console.log(soyadınız);
+console.log(doğumYeri);
+console.log(medeniDurum);
+console.log(yaşınız);
+console.log(doğumYılınız);
+console.log(doğumTarihiniz);
+
+
